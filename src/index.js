9dd559832857a771100ca3bb4a56432d658e50f0
maxH4k5s <rohitbhatnagar171@gmail.com>
2026-01-17 09:27:07 +0530

    dotenv setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import connectDB from "./db";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: "./env"
+})
+
+
+
+
+
+
+
+
+
+
+
 
 connectDB();
 
